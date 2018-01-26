0d84feca025f128d8e54c7b40009acc8eb9a7fb3
Atamas Ilya <atamas1llya@gmail.com>
2018-01-26 18:16:26 +0200

    Remove unnecessary random title component and move corresponding functions to app.js' componentWillMount()

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/main.css';
 import apps from './apps.json';
 
+import { getRandomEmoji, getRandomTitle } from './window-title-builder';
+
 import AppGrid from './components/AppGrid';
-import RandomTitle from './components/RandomTitle';
 
-const App = () => (
-  <div>
-    <RandomTitle />
-    <AppGrid apps={apps} />
-  </div>
-)
+class App extends Component {
+  componentWillMount() {
+    const emoji = getRandomEmoji();
+    const title = getRandomTitle();
 
+    document.title = `${emoji} ${title}`;
+  }
+
+  render() {
+    return <AppGrid apps={apps} />;
+  }
+}
 const reactRoot = document.createElement('div');
 reactRoot.id = 'react-root';
 
--- src/components/AppTile.js
+++ src/components/AppTile.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 import './app-tile.css';
 
-export default class AppTile extends React.Component {
-  render() {
-    const { app } = this.props;
+const AppTile = ({ app }) => (
+  <div className="app-tile">
+    <a href={app.href}>
+      <img src={app.logo} />
+    </a>
+  </div>
+)
 
-    return (
-      <div className="app-tile">
-        <a href={app.href}>
-          <img src={app.logo} />
-        </a>
-      </div>
-    )
-  }
-}
+export default AppTile;
